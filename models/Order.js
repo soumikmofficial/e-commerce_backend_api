85c2a6f6b3af90d5cbae7c39839b352c06f7af77
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-12-06 01:46:55 +0530

    review model addtions and fixes

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,17 @@
 const mongoose = require("mongoose");
+
+const singleCartItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
 const reviewSchema = new mongoose.Schema({
   tax: {
     type: Number,
@@ -7,6 +20,12 @@ const reviewSchema = new mongoose.Schema({
   shippingFee: {
     type: Number,
     required: true,
+  },
+  cartItems: [singleCartItemSchema],
+  status: {
+    type: String,
+    enum: ["pending", "failed", "delivered", "canceled", "paid"],
+    default: "pending",
   },
   subtotal: {
     type: Number,
