a6510e7044114026236f82bbb93de8a4b4a0f1d8
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-12-07 11:09:25 +0530

    order creation logic complete

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
 const Order = require("../models/Order");
+const Product = require("../models/Product");
 const CustomError = require("../errors");
 const { checkPermissions } = require("../utils");
+
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = "someRandomValue";
+  return { client_secret, amount };
+};
 
 // ...................all orders...................
 const getAllOrders = async (req, res) => {
@@ -20,7 +26,59 @@ const getCurrentUserOrders = async (req, res) => {
 
 // ........................create order.........................
 const createOrder = async (req, res) => {
-  res.send("create orders");
+  const { tax, shippingFee, cartItems } = req.body;
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError(
+      `Cannot create order with an empty cart`
+    );
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(`Tax/ShippingFee is missing`);
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product with the id: ${item.product}`
+      );
+    }
+    const { name, image, price, _id } = dbProduct;
+    const singleOrderItem = {
+      name,
+      price,
+      image,
+      product: _id,
+      //amount is coming from front end
+      amount: item.amount,
+    };
+    orderItems = [...orderItems, singleOrderItem];
+    subtotal += item.amount * price;
+  }
+  // calculate total
+  const total = tax + shippingFee + subtotal;
+  // get client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "usd",
+  });
+
+  const order = await Order.create({
+    cartItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 
 // .....................update order.............................
